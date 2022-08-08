8aa3f3ebeb2b8fe46925c0224a8b2931c2af61a9
guillaume <quet.guillaume.leo@gmail.com>
2022-08-08 10:32:59 +0200

    adding threejs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path'
 
 export default defineConfig({
   root: './src/',
   build: {
-    outDir: '../'
+    outDir: '../',
+    lib: {
+      entry: resolve(__dirname, 'src/main.js'),
+      name: 'main',
+      // the proper extensions will be added
+      fileName: 'main'
+    },
   }
 })
