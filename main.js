900d14a20d0d30e924c1c0dee9cf0380337d741c
guillaume <quet.guillaume.leo@gmail.com>
2022-10-18 16:10:31 +0200

    adding custom loading

--- main.js
+++ main.js
@@ -2,11 +2,37 @@ import * as BABYLON from 'babylonjs';
 
 import 'babylonjs-loaders';
 
-function padLeadingZeros(num, size) {
-  var s = num + '';
-  while (s.length < size) s = '0' + s;
-  return s;
-}
+BABYLON.DefaultLoadingScreen.prototype.displayLoadingUI = function () {
+  if (document.getElementById('customLoadingScreenDiv')) {
+    // Do not add a loading screen if there is already one
+    document.getElementById('customLoadingScreenDiv').style.display = 'initial';
+    return;
+  }
+  this._loadingDiv = document.createElement('div');
+  this._loadingDiv.id = 'customLoadingScreenDiv';
+  this._loadingDiv.innerHTML = 'scene is currently loading';
+  var customLoadingScreenCss = document.createElement('style');
+  customLoadingScreenCss.type = 'text/css';
+  customLoadingScreenCss.innerHTML = `
+  #customLoadingScreenDiv{
+      background-color: #BB464Bcc;
+      color: white;
+      font-size:50px;
+      text-align:center;
+  }
+  `;
+  document.getElementsByTagName('head')[0].appendChild(customLoadingScreenCss);
+  this._resizeLoadingUI();
+  window.addEventListener('resize', this._resizeLoadingUI);
+  document.body.appendChild(this._loadingDiv);
+};
+
+BABYLON.DefaultLoadingScreen.prototype.hideLoadingUI = function () {
+  document.getElementById('customLoadingScreenDiv').style.display = 'none';
+  console.log('scene is now loaded');
+};
+
+let iFrame = 0;
 
 // Get the canvas DOM element
 var canvas = document.getElementById('renderCanvas');
@@ -101,9 +127,14 @@ var createScene = function () {
 
   scene.registerBeforeRender(() => {
     pbr.baseTexture = textureFiles[counter];
-    counter++;
+    if (iFrame % 2 == 0) counter++;
+    iFrame++;
     if (counter == 55) counter = 0;
   });
+
+  scene.onPointerDown = (event, pickResult) => {
+    console.log(event, pickResult);
+  };
 
   return scene;
 };
